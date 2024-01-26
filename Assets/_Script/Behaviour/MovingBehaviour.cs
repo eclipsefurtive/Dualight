26b10758a858c806e75e2112043fa024f50104bd
Jonathan <22larochj@gmail.com>
2024-01-26 10:37:35 +0100

    Moving behaviour direction

---
+++ Assets/_Script/Behaviour/MovingBehaviour.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using _Script.Utils;
+using TMPro;
+using UnityEngine;
+
+public class MovingBehaviour : MonoBehaviour
+{
+    [SerializeField] private Direction _direction;
+    [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _movementDistance = 5f;
+
+    private Vector3 _startPosition;
+    private Vector3 _directionVector;
+
+    private void Awake()
+    {
+        _startPosition = transform.position;
+    }
+    
+    void Update()
+    {
+        _directionVector = _direction switch
+        {
+            Direction.Up => Vector3.up,
+            Direction.Right => Vector3.right,
+            Direction.Down => Vector3.down,
+            Direction.Left => Vector3.left,
+            _ => Vector3.right
+        };
+        
+        Move(); 
+    }
+
+    private void Move()
+    {
+        float pingPongValue = Mathf.PingPong(Time.time * _moveSpeed, _movementDistance);
+        transform.position = _startPosition + _directionVector * pingPongValue;
+    }
+
+}
---
+++ Assets/_Script/Utils/Enums.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _Script.Utils
+{
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
